07dc12d74593154f335567c55c478541a4f86687
Adeyemi Omisakin <aomisakin@usf.edu>
2020-01-23 12:44:23 -0500

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // puase to display Hello World
+            Console.WriteLine("Press any Key to Continue...");
+            Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
+        }
+    }
+}
